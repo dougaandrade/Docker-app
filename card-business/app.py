5d85bd0fe5e987711eb24ff5f199868e8c106593
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2024-12-06 10:00:02 -0300

    refact♻️:app/html/sytles

--- card-business/app.py
+++ card-business/app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def exibir_cartao():
-    logo = os.environ.get('LOGOMARCA')
     foto = os.environ.get('FOTO')
     nome = os.environ.get('NOME')
     idade = os.environ.get('IDADE')
@@ -12,7 +11,7 @@ def exibir_cartao():
     profissao = os.environ.get('PROFISSAO')
     site = os.environ.get('SITE')
 
-    return render_template('cartao.html', logo=logo, foto=foto, nome=nome, idade=idade, email=email, profissao=profissao, site=site)
+    return render_template('cartao.html',foto=foto, nome=nome, idade=idade, email=email, profissao=profissao, site=site)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
